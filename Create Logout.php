c75a0929931d720ba92d3fb877fe51896647f850
Tashini Upendra <tashiniupendra@gmail.com>
2026-04-03 21:31:17 +0530

    Create Logout.php for session management

--- Create Logout.php
+++ Create Logout.php
@@ -1,6 +1,13 @@
-<?
+<?php
 session_start();
+
+// Remove all session variables
 session_unset();
-session_destory();
+
+// Destroy session
+session_destroy();
+
+// Redirect to login page
 header("Location: login.php");
 exit();
+?>
